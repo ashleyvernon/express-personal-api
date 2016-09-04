6bf45965a7c11d3fb9f1bc99fd9baa67158fe4d5
ashleyvernon <avernon526@gmail.com>
2016-09-03 20:50:26 -0700

    updated adventures in seed.js

--- seed.js
+++ seed.js
@@ -13,18 +13,37 @@ var ashleyVernon = {
 };
 
 var myAdventures = [{
-	location: "Rockbound Lake, Desolation Wildnerness",
-  imageLink: "http://i.imgur.com/d1wWTXJ.jpg?1",
-	date: "June 8th, 2016",
-	typeOfAdventure: "Backpacking",
-	lengthOfAdventure: "2 days, 14.5 miles"
-},
-{
 	location: "Yosemite, Ca",
 	date: "April 21, 2016",
 	typeOfAdventure: "Rock Climbing",
 	lengthOfAdventure: "2 days"
-}]
+},
+{
+  location: "Rockbound Lake, Desolation Wildnerness",
+  imageLink: "http://i.imgur.com/d1wWTXJ.jpg?1",
+  date: "June 8th, 2016",
+  typeOfAdventure: "Backpacking",
+  lengthOfAdventure: "2 days, 14.5 miles"
+},
+{
+  location: "Big Chief, Lake Tahoe",
+  date: "July 1st, 2016",
+  typeOfAdventure: "Rock Climbing",
+  lengthOfAdventure: "3 days"
+},
+{
+  location: "Russian River, Ca",
+  date: "July 9th, 2016",
+  typeOfAdventure: "River Rafting",
+  lengthOfAdventure: "1 day"
+},
+{
+  location: "Utica Reservior, Ca",
+  date: "July 15th, 2016",
+  typeOfAdventure: "Kayaking",
+  lengthOfAdventure: "3 days"
+}
+]
 
 
 
